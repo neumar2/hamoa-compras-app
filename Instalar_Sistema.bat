314d00e2ae046b764c0aa17a75e75a035e3f70f6
Neumar Porto Permonian <Analista@hamoa.alf>
2026-06-23 10:26:51 -0400

    fix: resolve infinite UAC loop in installer script

--- Instalar_Sistema.bat
+++ Instalar_Sistema.bat
@@ -4,8 +4,8 @@ chcp 65001 >nul
 title Instalador - Compras App
 
 :: 1. Solicita Privilégios de Administrador
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' (
+fsutil dirty query %systemdrive% >nul 2>&1
+if %errorlevel% neq 0 (
     echo =====================================================
     echo SOLICITANDO PRIVILEGIOS DE ADMINISTRADOR...
     echo =====================================================
@@ -14,8 +14,8 @@ if '%errorlevel%' NEQ '0' (
 
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/c """"%~s0""""", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    cscript //nologo "%temp%\getadmin.vbs"
     exit /B
 
 :gotAdmin
